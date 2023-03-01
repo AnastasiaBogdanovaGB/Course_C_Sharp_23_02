788b1997ebf40bef006b23e154285f114f8051be
Bogdanova Anastasya <nastasya.bogdanova.art@mail.ru>
2023-03-01 23:51:27 +0300

    Task_HW 1_2

---
+++ Lesson_1/HW/HW_1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+    a = b;
+}
+if (a < c)
+{
+    a = c;
+}
+
+Console.Write($"Это число {a} больше");
