285e2afb1f61226d4074ec02bbc8d16f4239a505
Bogdanova Anastasya <nastasya.bogdanova.art@mail.ru>
2023-03-05 21:33:32 +0300

    Task 2_3

--- Lesson_2/2_1/HW/HW_2_1/HW_2_3/Program.cs
+++ Lesson_2/2_1/HW/HW_2_1/HW_2_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("Нет третьего числа");
+        return;
+    }
+
+while (num > 999) num /=10;
+Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()!));
